7f32e03dc0b8e4c9ff563c301915e1820ccafc48
Joseda <Joseda@DESKTOP-25070J0>
2023-03-07 11:45:27 +0100

    Valor = 2

--- src/josedavidalvarez/git/JoseDavidAlvarezGIT.java
+++ src/josedavidalvarez/git/JoseDavidAlvarezGIT.java
@@ -16,7 +16,7 @@ public class JoseDavidAlvarezGIT {
     // José David Álvarez Sánchez
     public static void main(String[] args) {
         int valor;
-        valor = 1;
+        valor = 2;
         System.out.println("Valor = " + valor);
     }
     
