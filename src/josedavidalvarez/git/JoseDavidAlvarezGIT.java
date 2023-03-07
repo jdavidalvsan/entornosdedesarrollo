9833fa8e0079c630fa46f96abc495cbbb77b817a
Joseda <Joseda@DESKTOP-25070J0>
2023-03-07 11:36:38 +0100

    Version Inicial (valor = 1)

---
+++ src/josedavidalvarez/git/JoseDavidAlvarezGIT.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package josedavidalvarez.git;
+
+/**
+ *
+ * @author Josedavid
+ */
+public class JoseDavidAlvarezGIT {
+
+    /**
+     * @param args the command line arguments
+     */
+    // José David Álvarez Sánchez
+    public static void main(String[] args) {
+        int valor;
+        valor = 1;
+        System.out.println("Valor = " + valor);
+    }
+    
+}
